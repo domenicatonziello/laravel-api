681710a440249c1502df91dcebb1aa6e4e99285a
domenicatonziello <domenica.tonziello10@gmail.com>
2023-03-15 15:11:47 +0100

    first commit

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Exists;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $project = new Project();
+        $types = Type::all();
+        $technologies = Technology::select('id', 'label')->orderBy('id')->get();
+        return view('admin.projects.create', compact('project', 'types', 'technologies'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|unique:projects|max:50',
+            'description' => 'string|nullable',
+            'image' => 'image|nullable',
+            'link_project' => 'required|string|unique:projects',
+            'type_id' => 'nullable|exists:types,id',
+            'technologies' => 'nullable|exists:technologies,id'
+        ], [
+            'title.required' => 'il titolo è obbligatorio',
+            'title.max' => 'il titolo deve avere massimo 50 caratteri',
+            'title.unique' => "Esiste già un progetto $request->title ",
+            'link_project.required' => 'Il link per il progetto è obbligatorio',
+            'link_project.unique' => 'Link già esistente',
+            'type_id' => 'Il tipo selezionato non è valido',
+            'technologies' => 'La tecnologia selezionata non è valida'
+        ]);
+        $data = $request->all();
+        $project = new Project();
+
+        // controllo se nell'array ho l'immagine
+        if (array_key_exists('image', $data)) {
+            // prendo l'url dell'immagine
+            $img_url = Storage::put('projects', $data['image']);
+            // sostituisco url all'immagine
+            $data['image'] = $img_url;
+        }
+
+        $project->fill($data);
+        $project->save();
+
+        if (Arr::exists($data, 'technologies')) $project->technologies()->attach($data['technologies']);
+
+        return to_route('admin.projects.show', $project->id)->with('type', 'success')->with('message', 'Elemento creato con successo');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Project $project)
+    {
+        return view('admin.projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Project $project)
+    {
+        $types = Type::all();
+        $technologies = Technology::select('id', 'label')->orderBy('id')->get();
+
+        $proj_tech = $project->technologies->pluck('id')->toArray();
+
+        return view('admin.projects.edit', compact('project', 'types', 'technologies', 'proj_tech'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Project $project)
+    {
+        $request->validate([
+            'title' => ['required', 'string', Rule::unique('projects')->ignore($project->id), 'max:50'],
+            'description' => 'string|nullable',
+            'image' => 'image|nullable',
+            'link_project' => ['required', 'string', Rule::unique('projects')->ignore($project->id)],
+            'type_id' => 'nullable|exists:types,id',
+            'technologies' => 'nullable|exists:technologies,id'
+        ], [
+            'title.required' => 'il titolo è obbligatorio',
+            'title.max' => 'il titolo deve avere massimo 50 caratteri',
+            'title.unique' => "Esiste già un progetto $request->title ",
+            'link_project.required' => 'Il link per il progetto è obbligatorio',
+            'link_project.unique' => 'Link già esistente',
+            'type_id' => 'Il tipo selezionato non è valido',
+            'technologies' => 'La tecnologia selezionata non è valida'
+        ]);
+        $data = $request->all();
+
+        // controllo se nell'array ho l'immagine
+        if (array_key_exists('image', $data)) {
+
+            // controllo se ho già l'immagine e la elimino
+            if ($data['image']) Storage::delete($project->image);
+
+            // prendo l'url dell'immagine
+            $img_url = Storage::put('projects', $data['image']);
+            // sostituisco url all'immagine
+            $data['image'] = $img_url;
+        }
+
+        $project->update($data);
+
+        if (Arr::exists($data, 'technologies')) $project->technologies()->sync($data['technologies']);
+        else if (count($project->technologies)) $project->technologies()->detach();
+
+        return to_route('admin.projects.show', $project->id)->with('type', 'success')->with('message', 'Progetto modificato con successo');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Project $project)
+    {
+        // controllo se ho già l'immagine e la elimino
+        if ($project->image) Storage::delete($project->image);
+
+        if (count($project->technologies)) $project->technologies()->detach();
+
+        $project->delete();
+        return to_route('admin.projects.index')->with('type', 'danger')->with('message', "Il progetto '$project->title' è stato eliminato.");
+    }
+}
---
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Technology;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class TechnologyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $technologies = Technology::all();
+        return view('admin.technologies.index', compact('technologies'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $technology = new Technology();
+        return view('admin.technologies.create', compact('technology'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'label' => 'required|string|unique:technologies|max:20',
+            'color' => 'required|string|size:7'
+        ], [
+            'label.required' => 'Il nome è obbligatorio',
+            'label.unique' => 'Esiste già una tecnologia con questo nome',
+            'label.max' => 'Il nome deve avere massimo :max caratteri',
+            'color.required' => 'Il colore è obbligatorio',
+            'color.size' => 'Il colore deve essere un codice esadecimale con cancelletto'
+        ]);
+
+        $data = $request->all();
+        $technology = new Technology();
+
+        $technology->fill($data);
+        $technology->save();
+
+        return to_route('admin.technologies.show', $technology->id)->with('type', 'success')->with('message', 'Elemento creato con successo');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Technology $technology)
+    {
+        return view('admin.technologies.show', compact('technology'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Technology $technology)
+    {
+        return view('admin.technologies.edit', compact('technology'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Technology $technology)
+    {
+        $request->validate([
+            'label' => ['required', 'string', Rule::unique('technologies')->ignore($technology->id), 'max:20'],
+            'color' => 'required|string|size:7'
+        ], [
+            'label.required' => 'Il nome è obbligatorio',
+            'label.unique' => 'Esiste già una tecnologia con questo nome',
+            'label.max' => 'Il nome deve avere massimo :max caratteri',
+            'color.required' => 'Il colore è obbligatorio',
+            'color.size' => 'Il colore deve essere un codice esadecimale con cancelletto'
+        ]);
+
+        $data = $request->all();
+
+        $technology->fill($data);
+        $technology->save();
+
+        return to_route('admin.technologies.show', $technology->id)->with('type', 'success')->with('message', 'Elemento modificato con successo');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Technology $technology)
+    {
+        $technology->delete();
+        return to_route('admin.technologies.index')->with('type', 'danger')->with('message', "L'elemento è stato eliminato.");
+    }
+}
